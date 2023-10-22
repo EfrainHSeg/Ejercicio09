53a8a0161014cea31fd20996b15e0c2e97d0a4ed
EfrainHSeg <efrainhsgi@gmail.com>
2023-10-21 20:12:39 -0500

    Cambios

--- Ejercicio09/Ejercicio09/App.xaml.cs
+++ Ejercicio09/Ejercicio09/App.xaml.cs
@@ -5,12 +5,13 @@ using Xamarin.Forms.Xaml;
 namespace Ejercicio09
 {
     public partial class App : Application
+
     {
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
